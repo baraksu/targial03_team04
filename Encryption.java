d7a1e00f409e830a82c4ab7b4299885a7acc1f1b
yehonatan351-dev <yehonatan351@gmail.com>
2025-12-02 18:38:41 +0200

    Update Encryption.java

--- Encryption.java
+++ Encryption.java
@@ -34,4 +34,64 @@ public class Encryption
         }
         return sentence;
     }
+    //move the characters to right-stern part
+    public static String MoveRight(String sentence,int words){
+        int len = sentence.length();
+        if(words==1){
+            String lastchar = sentence.substring(len-1);
+            String restofchar = sentence.substring(0,len-1);
+            return lastchar+restofchar;
+        }else{
+            if(words==2){
+                String lasttwochar = sentence.substring(len-2);
+                String restofchar = sentence.substring(0,len-2);
+                return lasttwochar+restofchar;
+            }else{
+                 if(words==3){
+                String lastthreechar = sentence.substring(len-3);
+                String restofchar = sentence.substring(0,len-3);
+                return lastthreechar+restofchar;
+            }else{
+                System.out.println(sentence+"contains more than 3 words");
+            }
+            }
+        }
+
+        return sentence;
+        }
+        //move the characters back to left-stern part
+ public static String MoveLeft(String sentence,int words){
+        if(words==1){
+            String lastchar = sentence.substring(0,1);
+            String restofchar=sentence.substring(1);
+            sentence=restofchar + lastchar;
+        }else if(words==2){
+            String lasttwochar = sentence.substring(0,2);
+            String restofchar=sentence.substring(2);
+            sentence=restofchar + lasttwochar;
+        }else if(words==3){
+            String lastthreechar = sentence.substring(0,3);
+            String restofchar=sentence.substring(3);
+            sentence=restofchar + lastthreechar;
+        }
+        return sentence;
+    }
+      //replacing vowels to special characters-Moishie's part
+    public static String ReplaceVowels(String sentence){
+        String result = sentence.replace('a','@');
+        result = sentence.replace('e','#');
+        result = sentence.replace('i','1');
+        result = sentence.replace('o','0');
+        result = sentence.replace('u','&');
+        return result;
+    }
+    //replacing special characters to vowels-Moishie's part
+    public static String ReplaceSpecialCharacter(String sentence){
+        String result = sentence.replace('@','a');
+        result = sentence.replace('#','e');
+        result = sentence.replace('1','i');
+        result = sentence.replace('0','o');
+        result = sentence.replace('&','u');
+        return result;
+    }
 }
